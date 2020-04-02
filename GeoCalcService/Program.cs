d6e7704833d18a0e79d9f5948acabf3c3a553996
Mojtaba Goodarzi <mojtaba73.g@outlook.com>
2020-04-03 01:01:49 +0430

    microservice projects added

---
+++ GeoCalcService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeoCalcService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
