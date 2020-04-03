222a5f48a9f02e7c2e2b7e331926fe705380f511
Mojtaba Goodarzi <mojtaba73.g@outlook.com>
2020-04-03 19:37:32 +0430

    range checking in viewmodel

--- AuthService/ViewModels/GeoPointsViewModel.cs
+++ AuthService/ViewModels/GeoPointsViewModel.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AuthService.ViewModels
 {
     public class GeoPointsViewModel
     {
+        [Range(-180, +180)]
         public double FromLat { get; set; }
+        [Range(-90, +90)]
         public double FromLong { get; set; }
-
+        [Range(-180, +180)]
         public double ToLat { get; set; }
+        [Range(-90, +90)]
         public double ToLong { get; set; }
 
         public override string ToString()
